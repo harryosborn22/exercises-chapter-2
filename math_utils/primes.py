06c6a4ecd2e72fbc41556500222ace7cd224dd64
harryosborn22 <harry.osborn22@imperial.ac.uk>
2026-04-20 18:49:52 +0100

    2.4 complete

---
+++ math_utils/primes.py
@@ -0,0 +1,12 @@
+from math import sqrt,ceil
+
+def isprime(x):
+    if x == 1:
+        return False
+    elif x == 2:
+        return True
+    else:
+        for i in range(2,ceil(sqrt(x)) + 1):
+            if x%i == 0:
+                return False
+        return True
